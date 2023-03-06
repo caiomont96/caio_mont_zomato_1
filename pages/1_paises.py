c98a112fc253e6cb4bfbf2b47a73f8dfdeb8e6ab
caiomont96 <114825022+caiomont96@users.noreply.github.com>
2023-03-06 13:48:43 -0300

    Update 1_paises.py

--- pages/1_paises.py
+++ pages/1_paises.py
@@ -218,10 +218,10 @@ with col1:
 with col2:
     st.subheader( 'Com maior avaliação média')
     
-    st.dataframe(df.style.format(subset=['Aggregate rating'], formatter='{:.2f}'))
+    #st.dataframe(df.style.format(subset=['Aggregate rating'], formatter='{:.2f}'))
 
  
-    maior_avaliacao = round(df.loc[:,['Country name','Aggregate rating']].groupby('Country name').mean().sort_values(['Aggregate rating'], ascending=False),1)
+    maior_avaliacao = (df.loc[:,['Country name','Aggregate rating']].groupby('Country name').mean().sort_values(['Aggregate rating'], ascending=False)
                                 
    
     st.dataframe(round(maior_avaliacao),2)
