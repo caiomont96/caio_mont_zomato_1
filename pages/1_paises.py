63ff1b8e3b90ad4343a8ed9c6e8fab6f3b00ca03
Caio <114825022+caiomont96@users.noreply.github.com>
2023-03-08 09:19:21 -0300

    Update 1_paises.py

--- pages/1_paises.py
+++ pages/1_paises.py
@@ -11,22 +11,15 @@ import folium
 from folium.plugins import MarkerCluster
 
 
-
-
 #df_raw = pd.read_csv('zomato_1.csv', encoding='ISO-8859-1')
 
 #from google.colab import drive
 
 #drive.mount('/content/drive')
 
-#df_raw = pd.read_csv('dataset/zomato_1.csv', encoding="ISO-8859-1")
-df_raw = pd.read_csv('dataset/zomato_1.csv', encoding='utf-8')
-
+df_raw = pd.read_csv('zomato_1.csv', encoding="utf-8")
 
 df = df_raw.copy()
-
-#st.write(df.style.format({"Predictions": "{:.2f}"}))
-
 
 # ===================================================================
 
@@ -104,14 +97,14 @@ st.sidebar.image( image, width=120 )
 
 
 st.sidebar.title( '' )
-st.sidebar.markdown( '## Filtro ' )
+st.sidebar.markdown( '## Filtros ' )
 
 st.sidebar.markdown( """___""" )
 
-st.sidebar.markdown( '## Seleção de países por exclusão')
+st.sidebar.markdown( '## Selecione um país')
 
 country_options = st.sidebar.multiselect(
-    'Quais países?',
+    'Qual país?',
     ['India','Australia','Brazil','Canada','Indonesia',"New Zeland","Philippines","Qatar","Singapure","South Africa","Sri Lanka","Turkey","United Arab Emirates","England","United States of America"],
     default= ['India','Australia','Brazil','Canada','Indonesia',"New Zeland","Philippines","Qatar","Singapure","South Africa","Sri Lanka","Turkey","United Arab Emirates","England","United States of America"] )
 
@@ -123,58 +116,46 @@ df = df.loc[linhas_selecionadas, :]
 
 #-------------------------------------------------------------------------------------
 
-    #-------------------------------------------------------------------
-#Qual o país que mais aceita reserva?
+#-------------------------------------------------------------------
 
-#with st.container():
-    
-  #  st.markdown('Países que mais aceitam reserva')
-       
-  #  df_tbooking = df[df['Has Table booking'].isin([1])]
+   
+with st.container():
+    st.markdown("""___""")
+    st.title( 'Ranking de avaliações' )
 
-   # reserva = df_tbooking.loc[:,['Country name','Has Table booking']].groupby('Country name').count().sort_values(['Has Table booking'], ascending=False)
+col1, col2 = st.columns( 2 )
+with col1:
+    st.subheader( 'Total de avaliações por país')
+ 
+    mais_avaliacao = (df.loc[:,['Country name','Votes']].groupby('Country name').sum().sort_values(['Votes'], ascending=False))
+                               
+   
+    st.dataframe(mais_avaliacao)
+   
+with col2:
+    st.subheader( 'Avaliação média por país')
+ 
+    maior_avaliacao = round(df.loc[:,['Country name','Aggregate rating']].groupby('Country name').mean().sort_values(['Aggregate rating'], ascending=True),2)
     
+    maior_avaliacao = maior_avaliacao.reset_index()
 
-
- #   reserva = reserva.reset_index()
-    
- #   reserva.head()
-    
-    #st.plotly_chart( fig, use_container_width=True )
-    
-    #-------------------------------------------------------------------
-
-        #------------------------------------------------------------------
-    
+    fig = px.bar(maior_avaliacao, x= 'Aggregate rating', y= 'Country name', orientation='h')
+                               
+   
+    st.plotly_chart( fig, use_container_width=True )
+   
+ 
 
 # ------------------------------------------------------------------------
 
-with st.container():
-       
-    col1, col2, col3, col4 = st.columns(4)
-    with col1:      
-        rstrts = df.loc[:, 'Restaurant ID'].nunique()
-        col1.metric('Restaurantes', rstrts)
-           
-    with col2:
-        paises_registrados = df.loc[:,	'Country Code'].nunique()
-        col2.metric('Países', paises_registrados)
-           
-    with col3:
-        cidade = df.loc[:,	'City'].nunique()
-        col3.metric('Cidades', cidade)
-
-    with col4:
-        total_avaliacoes = df['Votes'].sum()
-        col4.metric('Total Avaliações', total_avaliacoes) 
-
 # Mapa!
-    
+   
 with st.container():
-    
+   
         st.markdown("""___""")
-        st.subheader('Localização geográfica dos restaurantes')
-        st.markdown('*zoom in/out com scroll do mouse')
+        st.title('Localização dos restaurantes')
+        st.markdown('*zoom in/out com scroll do mouse ou touch')
+
         df_ll = df.loc[:,['Restaurant ID','Latitude','Longitude']]
 
         df_ll = df_ll.rename(columns={"Restaurant ID":"ID",'Latitude':'Y','Longitude':'X'})
@@ -182,60 +163,37 @@ with st.container():
         df_ll.columns = df_ll.columns.str.strip()
 
         #df_ll.head()
-        
+       
         subset_of_df_ll = df_ll.sample(n=500)
-        
-        
+       
+       
         some_map2 = folium.Map(location= [subset_of_df_ll['Y'].mean(),
                                 subset_of_df_ll['X'].mean()],
                                 zoom_start=10)
-        
+       
         mc= MarkerCluster()
-        
+       
         for row in subset_of_df_ll.itertuples():
 
             mc.add_child(folium.Marker(location=[row.Y,row.X], popup=row.ID))
-            
+           
         some_map2.add_child(mc)
-        
+       
         folium_static(some_map2, width=700, height=500)
-      
+
+        #-----------------------------------------
 
 # Países com mais restaurantes cadastrados
-
-with st.container():
-    st.markdown("""___""")
-
-col1, col2 = st.columns( 2 )
-with col1:
-    st.subheader( 'Mais avaliados')
- 
-    mais_avaliacao = (df.loc[:,['Country name','Votes']].groupby('Country name').sum().sort_values(['Votes'], ascending=False))
-                                
-   
-    st.dataframe(mais_avaliacao)
-   
-with col2:
-    st.subheader( 'Com maior avaliação média')
-    
-    #st.dataframe(df.style.format(subset=['Aggregate rating'], formatter='{:.2f}'))
-
- 
-    maior_avaliacao = round(df.loc[:,['Country name','Aggregate rating']].groupby('Country name').mean().sort_values(['Aggregate rating'], ascending=False),2)
-                                
-   
-    st.dataframe(maior_avaliacao)
-
 
 with st.container():
     st.markdown("""___""")
     st.title( 'Características' )
 
 with st.container():
-    
+   
     st.markdown('## Países com mais restaurantes cadastrados')
-    
-
+   
+#----------------------------------------------------------------
 
     df_aux = df.loc[:, ['City','Country name']].groupby('Country name').count().sort_values(['City'], ascending=False)
 
@@ -252,9 +210,9 @@ with st.container():
 #Qual o país que possui na média a maior nota média registrada?
 
 with st.container():
-    
-    st.markdown('## Países com restaurantes que mais aceitam reserva')
-    
+   
+    st.markdown('## Países que possuem os restaurantes que mais aceitam reserva')
+   
     df_tbooking = df[df['Has Table booking'].isin([1])]
        
     reserva = df_tbooking.loc[:,['Country name','Has Table booking']].groupby('Country name').count().sort_values(['Has Table booking'], ascending=False)
@@ -265,14 +223,24 @@ with st.container():
     fig = px.bar(reserva, x= 'Country name', y= 'Has Table booking')
 
     #fig.show()
-    
+   
     st.plotly_chart( fig, use_container_width=True )
-
-    #----------------------------------
     
-   # st.subheader( 'Países que mais aceitam reserva' )
-  #  df_tbooking = df[df['Has Table booking'].isin([1])]
+#---------------------------------------------------------------------------------
 
-  #  reserva = df_tbooking.loc[:,['Country name','Has Table booking']].groupby('Country name').count().sort_values(['Has Table booking'], ascending=False)
-  
-  #  st.dataframe(reserva)
+with st.container():
+   
+    st.markdown('## Países que possuem restaurantes que realizam pedidos de forma online')
+   
+    df_tbooking = df[df['Has Online delivery'].isin([1])]
+       
+    online = df_tbooking.loc[:,['Country name','Has Online delivery']].groupby('Country name').count().sort_values(['Has Online delivery'], ascending=False)
+
+
+    online = online.reset_index()
+
+    fig = px.pie(online, values='Has Online delivery', names ='Country name' )
+
+    #fig.show()
+   
+    st.plotly_chart( fig, use_container_width=True )
